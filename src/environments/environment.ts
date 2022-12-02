1ba9728c677773d27198b09e6919141bd5f5c6db
GustavoMaLem30 <gumarinle@ittepic.edu.mx>
2022-12-01 23:34:52 -0700

    Preparacion examen

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAcMulNJSo5OFVUS0LCQPVDSNpQe9E03Ac",
+    authDomain: "dah-examen-u4-gl-eh.firebaseapp.com",
+    projectId: "dah-examen-u4-gl-eh",
+    storageBucket: "dah-examen-u4-gl-eh.appspot.com",
+    messagingSenderId: "876720670976",
+    appId: "1:876720670976:web:bc39bdf279c6d9691c8be5"
+  }
 };
 
 /*
